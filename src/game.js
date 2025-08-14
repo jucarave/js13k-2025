f975f5397f2d14ae9e9fce57cc85fa2a5e775582
Camilo Ramírez <jucarave92@hotmail.com>
2025-08-14 00:16:36 -0500

    feat: load room walls geometry
    Right now it lives inside the js file, I might move the specification of
    the walls to a binary file if it gets heavy in bytes

--- src/game.js
+++ src/game.js
@@ -30,7 +30,9 @@ cr_false = false,
 cr_Math = Math,
 cr_Mathcos = cr_Math.cos,
 cr_Mathsin = cr_Math.sin,
-cr_document = document;
+cr_document = document,
+
+cr_room = [0, 0, 9, 0, 13, 7, 13, 12, 6, 12, 6, 8, 0, 8, 0, 0]; // Room coordinates set by x, y pairs
 
 /**
  * SECTION VARIABLES
@@ -43,18 +45,19 @@ let cr_canvas,      // HTMLCanvasElement
 
   cr_keys = {},     // Object to store key states
 
-  cr_cameraPosition = [0,0, -3],          // Camera position in 3D space
+  cr_getUniformLocation = null, // Function to get uniform locations
+  cr_uniformMatrix4fv = null,   // Function to set uniform matrices
+
+  cr_cameraPosition = [0,-0.5, -3],       // Camera position in 3D space
   cr_cameraRotation = 0,                  // Camera rotation angle in radians
-  cr_cameraView  = cr_matTranslate(),     // Camera view matrix         
+  cr_cameraView = cr_updateMatrix(cr_cameraPosition, cr_cameraRotation),     // Camera view matrix         
   cr_cameraProjection  = [1.732, 0, 0, 0, // Camera projection matrix
                           0, 3.079, 0, 0, 
                           0, 0, -1, -1, 
                           0, 0, -0.1, 0],
   
-  cr_quadVertexBuffer = null, // Vertex Buffer Object for quad
-  cr_quadIndexBuffer = null,  // Index Buffer Object for quad
-  cr_quadRotation = 0,
-  cr_quadMatriz = cr_matTranslate();
+  cr_identityMatrix = cr_matTranslate(), // Static identity matrix for transformations
+  cr_geometries = { cr_room: {} };       // Object to hold geometries for different objects in the game
 
 /**
  * SECTION UTILITY FUNCTIONS
@@ -87,20 +90,22 @@ function cr_matMultiply(A, B) {
   return cr_matriz;
 }
 
-function cr_updateMatrix(cr_position, cr_rotation) {
+function cr_updateMatrix(cr_position, cr_rotation, cr_transMat = true) {
   const cr_cos = cr_Mathcos(cr_rotation);
   const cr_sin = cr_Mathsin(cr_rotation);
   
   // Let's multiply the rotation and translation matrices
-  return cr_matMultiply(
-    cr_matTranslate(cr_position[0], cr_position[1], cr_position[2]),
-    [
+  const cr_rot = [
       cr_cos, 0, cr_sin, 0,
       0, 1, 0, 0,
       -cr_sin, 0, cr_cos, 0,
       0, 0, 0, 1
-    ]
-  );
+    ];
+  const cr_trans = cr_matTranslate(cr_position[0], cr_position[1], cr_position[2]);
+
+  return cr_transMat ? 
+    cr_matMultiply(cr_trans, cr_rot) : 
+    cr_matMultiply(cr_rot, cr_trans);
 }
 
 /**
@@ -108,18 +113,20 @@ function cr_updateMatrix(cr_position, cr_rotation) {
  */
 function cr_updateCamera() {
   // Update camera rotation based on input
-  const r = 0.05;
-  cr_cameraRotation += cr_keys["ArrowLeft"] ? r : cr_keys["ArrowRight"] ? -r : 0;
+  const R = 0.05;
+  const T = cr_keys["ArrowLeft"] ? 1 : cr_keys["ArrowRight"] ? -1 : 0;
+  cr_cameraRotation += T * R;
 
   // Update camera position based on input
-  const c = cr_Mathcos(cr_cameraRotation);
-  const s = cr_Mathsin(cr_cameraRotation);
-  const v = 0.1;
-  const m = (cr_keys["ArrowUp"] ? 1 : cr_keys["ArrowDown"] ? -1 : 0);
-  cr_cameraPosition[0] += m * s * v;
-  cr_cameraPosition[2] += m * c * v;
+  const C = cr_Mathcos(cr_cameraRotation);
+  const S = cr_Mathsin(cr_cameraRotation);
+  const V = 0.1;
+  const M = (cr_keys["ArrowUp"] ? 1 : cr_keys["ArrowDown"] ? -1 : 0);
+  cr_cameraPosition[0] += M * S * V;
+  cr_cameraPosition[2] += M * C * V;
 
-  cr_cameraView = cr_updateMatrix(cr_cameraPosition, cr_cameraRotation);
+  // If there was any movement, update the camera view matrix
+  (M || T) && (cr_cameraView = cr_updateMatrix(cr_cameraPosition, cr_cameraRotation));
 }
 
 /**
@@ -137,6 +144,9 @@ function cr_initEngine() {
   cr_gl.enable(2929);   // Enable depth testing 
   cr_gl.depthFunc(515); // Set the depth function to gl.LEQUAL
   cr_gl.viewport(0, 0, cr_width, cr_height);
+
+  cr_getUniformLocation = cr_gl.getUniformLocation.bind(cr_gl);
+  cr_uniformMatrix4fv = cr_gl.uniformMatrix4fv.bind(cr_gl);
 
   // Load and compile the shader
   const cr_shaders = [];
@@ -159,62 +169,70 @@ function cr_initEngine() {
   );
 }
 
-function cr_createQuad() {
-  const cr_positions = new Float32Array([
-    -0.5, -0.5, 0,
-     0.5, -0.5, 0,
-    -0.5,  0.5, 0,
-     0.5,  0.5, 0
-  ]);
-  const cr_indices = new Uint16Array([
-    0, 1, 2,
-    2, 1, 3
-  ]);
-
-  cr_quadVertexBuffer = cr_gl.createBuffer();
-  cr_gl.bindBuffer(34962, cr_quadVertexBuffer); // 34962 is for ARRAY_BUFFER
-  cr_gl.bufferData(34962, cr_positions, 35044); // 35044 is for STATIC_DRAW
-
-  cr_quadIndexBuffer = cr_gl.createBuffer();
-  cr_gl.bindBuffer(34963, cr_quadIndexBuffer);  // 34963 is for ELEMENT_ARRAY_BUFFER
-  cr_gl.bufferData(34963, cr_indices, 35044);   // 35044 is for STATIC_DRAW
-}
-
-function cr_renderQuad() {
-  cr_gl.bindBuffer(34962, cr_quadVertexBuffer); // 34962 is for ARRAY_BUFFER
-  cr_gl.bindBuffer(34963, cr_quadIndexBuffer);  // 34963 is for ELEMENT_ARRAY_BUFFER
+/**
+ * SECTION GEOMETRY FUNCTIONS
+ */
+function cr_renderGeometry(cr_geometry, cr_worldMatrix) {
+  cr_gl.bindBuffer(34962, cr_geometry.cr_vertexBuffer); // 34962 is for ARRAY_BUFFER
+  cr_gl.bindBuffer(34963, cr_geometry.cr_indexBuffer);  // 34963 is for ELEMENT_ARRAY_BUFFER
 
   const cr_positionLocation = cr_gl.getAttribLocation(cr_program, "a_p");
   cr_gl.enableVertexAttribArray(cr_positionLocation);
   cr_gl.vertexAttribPointer(cr_positionLocation, 3, 5126, cr_false, 12, 0); // 5126 is for FLOAT, 12 is stride (3 floats * 4 bytes each)
 
   // Set the camera view and projection matrices
-  const cr_cameraViewLocation = cr_gl.getUniformLocation(cr_program, "u_cw");
-  const cr_cameraProjectionLocation = cr_gl.getUniformLocation(cr_program, "u_cp");
-  cr_gl.uniformMatrix4fv(cr_cameraViewLocation, cr_false, cr_cameraView);
-  cr_gl.uniformMatrix4fv(cr_cameraProjectionLocation, cr_false, cr_cameraProjection);
+  const cr_cameraViewLocation = cr_getUniformLocation(cr_program, "u_cw");
+  const cr_cameraProjectionLocation = cr_getUniformLocation(cr_program, "u_cp");
+  cr_uniformMatrix4fv(cr_cameraViewLocation, cr_false, cr_cameraView);
+  cr_uniformMatrix4fv(cr_cameraProjectionLocation, cr_false, cr_cameraProjection);
 
-  // Update quad rotation matrix
-  const cr_quadModelLocation = cr_gl.getUniformLocation(cr_program, "u_m");
-  cr_gl.uniformMatrix4fv(cr_quadModelLocation, cr_false, cr_quadMatriz);
+  // Update rotation matrix
+  const cr_quadModelLocation = cr_getUniformLocation(cr_program, "u_m");
+  cr_uniformMatrix4fv(cr_quadModelLocation, cr_false, cr_worldMatrix);
 
-  cr_gl.drawElements(4, 6, 5123, 0); // 4 is for TRIANGLES, 6 is the number of indices, 5123 is for UNSIGNED_SHORT
+  cr_gl.drawElements(4, cr_geometry.cr_indices.length, 5123, 0); // 4 is for TRIANGLES, 6 is the number of indices, 5123 is for UNSIGNED_SHORT
+}
+
+function cr_buildRoomGeometry(cr_geometry, cr_room) {
+  cr_geometry.cr_vertices = [];
+  cr_geometry.cr_indices = [];
+  let cr_indexOffset = 0;
+  for (let i=0;i<cr_room.length-2;i+=2) {
+    const x1 = cr_room[i], y1 = cr_room[i+1];
+    const x2 = cr_room[i+2], y2 = cr_room[i+3];
+
+    // Add two triangles for each rectangle defined by the coordinates
+    cr_geometry.cr_vertices.push(
+      x1, 0, y1,
+      x2, 0, y2,
+      x1, 1, y1,
+      x2, 1, y2
+    );
+
+    cr_geometry.cr_indices.push(
+      cr_indexOffset + 0, cr_indexOffset + 1, cr_indexOffset + 2,
+      cr_indexOffset + 2, cr_indexOffset + 1, cr_indexOffset + 3
+    );
+    cr_indexOffset += 4;
+  }
+
+  cr_geometry.cr_vertexBuffer = cr_gl.createBuffer();
+  cr_gl.bindBuffer(34962, cr_geometry.cr_vertexBuffer); // 34962 is for ARRAY_BUFFER
+  cr_gl.bufferData(34962, new Float32Array(cr_geometry.cr_vertices), 35044); // 35044 is for STATIC_DRAW
+
+  cr_geometry.cr_indexBuffer = cr_gl.createBuffer();
+  cr_gl.bindBuffer(34963, cr_geometry.cr_indexBuffer);  // 34963 is for ELEMENT_ARRAY_BUFFER
+  cr_gl.bufferData(34963, new Uint16Array(cr_geometry.cr_indices), 35044);   // 35044 is for STATIC_DRAW
 }
 
 /**
  * SECTION GAME LOOP
  */
-function cr_updateQuad() {
-  cr_quadRotation += 0.03;
-  cr_quadMatriz = cr_updateMatrix([0,0,0], cr_quadRotation);
-}
-
 function cr_update() {
   cr_gl.clear(16640); // 16640 is for COLOR_BUFFER_BIT | DEPTH_BUFFER_BIT
 
   cr_updateCamera();
-  cr_updateQuad();
-  cr_renderQuad();
+  cr_renderGeometry(cr_geometries.cr_room, cr_identityMatrix);
 
   requestAnimationFrame(cr_update);
 }
@@ -226,7 +244,7 @@ function cr_update() {
  */
 function cr_main() {
   cr_initEngine();
-  cr_createQuad();
+  cr_buildRoomGeometry(cr_geometries.cr_room, cr_room);
   cr_update();
 }
 
